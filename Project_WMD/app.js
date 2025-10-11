b3a8b24bf9b45a99350311e3f784f7fa40064097
jannesclaes <jannes.claes@student.ehb.be>
2025-10-11 18:43:31 +0200

    Get halte De lijn data

--- Project_WMD/app.js
+++ Project_WMD/app.js
@@ -32,3 +32,29 @@ walk.onclick = function(){
 submitBtn.onclick =function(){
     console.log(`je reis met de ${transport} van ${inputDuration.value} kilometer duurde ${inputDistance.value} minuten.`)
 }
+
+
+async function getLijnHalteData() {
+  const url = "https://api.delijn.be/DLZoekOpenData/v1/zoek/haltes/*?huidigePositie=51.0299814,4.9740799&maxAantalHits=3";
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Ocp-Apim-Subscription-Key": "e0e62df8f3514026b63deb400729c080", // <-- zet je echte key hier
+        "Accept": "application/json"
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error("Fout bij ophalen:", error.message);
+  }
+}
+
+getLijnHalteData()
